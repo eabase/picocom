1bf977fcac4fdaa6bba1787223e52f4af81a262f
Wolfram Sang <wsa@kernel.org>
2024-07-29 23:35:40 +0200

    release.sh: only change first VERSION var in Makefile
    Update the regex so it will only match the first VERSION assignment.
    
    Signed-off-by: Wolfram Sang <wsa@kernel.org>

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 # picocom release script by Wolfram Sang, see LICENSE.txt
 tag="$(date +%Y-%m)"
-sed -i "/^VERSION/ { s/=.*/= $tag/ }" Makefile Android.mk
+sed -i "/^VERSION :\?= 2/ { s/=.*/= $tag/ }" Makefile Android.mk
 cat > CONTRIBUTORS <<EOF
 These people have contributed to picocom according to the repository at
 https://gitlab.com/wsakernel/picocom. See also there for details about their
